afab7116c85c8b44fc26d780053b5968dbd9f807
Benoit Quette <benoit.quette@gmail.com>
2016-11-28 10:43:06 +0100

    added yarn

--- app.js
+++ app.js
@@ -6,15 +6,9 @@
 
 const express = require('express');
 const compression = require('compression');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const fallback = require('express-history-api-fallback');
-const webpackConfig = require('./webpack/webpack.config');
-const scheduler = require('./bin/scheduler');
-const projects = require('./routes/projects');
+const projects = require('./routes/Projects');
 require('./models/init');
 const passport = require('passport');
 const BasicStrategy = require('passport-http').BasicStrategy;
@@ -35,57 +29,16 @@ passport.use(new BasicStrategy(
   }
 ));
 
-// common paths
-const root = path.join(__dirname, 'public');
-const defaultDocName = 'index.html';
-const defaultDoc = path.join(root, defaultDocName);
-
 // Express setup
 var app = express();
 app.use(passport.initialize());
 app.use(compression());
-app.use(favicon(path.join(root, 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use('/api/projects', projects);
-if (process.env.NODE_ENV === 'development') {
-  app.use(express.static(root));
-  app.use(logger('dev'));
-  const webpack = require('webpack');
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const compiler = webpack(webpackConfig);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false,
-      noInfo: false,
-      quiet: false
-    }
-  });
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-  app.use(fallback(defaultDocName, {root}));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(defaultDoc));
-    res.end();
-  });
-} else {
-  if (process.env.NODE_ENV === 'production') {
-    app.use(passport.authenticate('basic', {session: false}));
-  }
-  app.use(express.static(root));
-  app.use(fallback(defaultDocName, {root}));
-  app.get('*',
-    function response(req, res) {
-      res.sendFile(defaultDoc);
-    });
+if (process.env.NODE_ENV === 'production') {
+  app.use(passport.authenticate('basic', {session: false}));
 }
 
 // catch 404 and forward to error handler
@@ -94,9 +47,5 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// start the scheduler that will trigger the audits according to the pattern
-// define in the config file
-scheduler.start();
 
 module.exports = app;
