2cc3844edc259829a84c027c916a578afa514176
Benoit Quette <ben@MacBook-de-Sara.local>
2016-12-07 13:38:20 +0100

    fixed require and remoived passport

--- app.js
+++ app.js
@@ -8,38 +8,18 @@ const express = require('express');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const projects = require('./routes/Projects');
+const projects = require('./routes/projects');
 require('./models/init');
-const passport = require('passport');
-const BasicStrategy = require('passport-http').BasicStrategy;
 
 // end module dependencies
 
-// passport config
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    console.log('passport....');
-    if (username !== 'guest') {
-      return done(null, false, {message: 'Incorrect username.'});
-    }
-    if (password !== 'beta2016') {
-      return done(null, false, {message: 'Incorrect password.'});
-    }
-    return done(null, {user: username});
-  }
-));
-
 // Express setup
 var app = express();
-app.use(passport.initialize());
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use('/api/projects', projects);
-if (process.env.NODE_ENV === 'production') {
-  app.use(passport.authenticate('basic', {session: false}));
-}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
