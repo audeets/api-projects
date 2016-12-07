a039662f6fad9dbc3684707fb7273c21436a0d76
Benoit Quette <ben@MacBook-de-Sara.local>
2016-12-07 17:13:10 +0100

    added CORS support

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api/projects', projects);
 
 // catch 404 and forward to error handler
