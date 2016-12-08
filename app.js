adc7134132c9cb00759f879056e2c51cbe4fefe8
Benoit Quette <ben@MacBook-de-Sara.local>
2016-12-08 18:26:42 +0100

    added CORS methods accept headers

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+    "POST, GET, PUT, DELETE");
   next();
 });
 app.use('/api/projects', projects);
