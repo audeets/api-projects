5f89b7beb4c814f05440906ff6467a120cd1e3a2
Benoit Quette <benoit.quette@gmail.com>
2024-02-06 17:19:33 +0100

    added project PUT

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const projects = require("./routes/projects");
 var app = express();
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_BASE_URL);
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
--- routes/projects.js
+++ routes/projects.js
@@ -189,24 +189,42 @@ router.route("/:id/audits").get(isUserAuthenticated, (req, res, next) => {
   });
 });
 
-router.route("/:id").delete(isUserAuthenticated, (req, res, next) => {
-  Project.deleteOne({ _id: req.params.id })
-    .then(() => {
-      return res.send({ id: req.params.id });
-    })
-    .catch((error) => {
-      return next(error);
-    });
-});
-
-router.route("/:id").get(isUserAuthenticated, (req, res, next) => {
-  Project.findOne({ _id: req.params.id })
-    .then((result) => {
-      return res.send(result);
-    })
-    .catch((error) => {
-      return next(error);
-    });
-});
+router
+  .route("/:id")
+  .delete(isUserAuthenticated, (req, res, next) => {
+    Project.deleteOne({ _id: req.params.id, user: req.user.id })
+      .then(() => {
+        return res.send({ id: req.params.id });
+      })
+      .catch((error) => {
+        return next(error);
+      });
+  })
+  .get(isUserAuthenticated, (req, res, next) => {
+    Project.findOne({ _id: req.params.id, user: req.user.id })
+      .then((result) => {
+        return res.send(result);
+      })
+      .catch((error) => {
+        return next(error);
+      });
+  })
+  .put(isUserAuthenticated, (req, res, next) => {
+    Project.findOne({ _id: req.params.id, user: req.user.id })
+      .then((result) => {
+        if (!result) {
+          return res.status(404).send("Project not found");
+        } else {
+          result.url = req.body.url;
+          result.title = req.body.title;
+          result.save().then(() => {
+            return res.status(200).json(result);
+          });
+        }
+      })
+      .catch((error) => {
+        return next(error);
+      });
+  });
 
 module.exports = router;
