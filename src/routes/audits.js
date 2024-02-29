1331e6b7ce6336f459ea9b170d1136acb2f2a962
Benoit Quette <benoit.quette@gmail.com>
2024-02-29 11:48:34 +0100

    remove unused, lodash and moment

--- src/routes/audits.js
+++ src/routes/audits.js
@@ -1,62 +1,12 @@
-import _ from "lodash";
 import elastic from "../utils/elastic.js";
-import moment from "moment";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat.js";
 import { isUserAuthenticated } from "@benoitquette/audeets-api-commons/middlewares/auth.js";
 
 const DATE_FORMAT = "YYYYMMDD";
+dayjs.extend(customParseFormat);
 
 const addRoutes = (router, baseRoute) => {
-  router.route(baseRoute).get(isUserAuthenticated, (req, res, next) => {
-    elastic("audits", { id: req.params.id }, (err, results) => {
-      if (err) return next(err);
-      const categories = results.aggregations.categories.buckets;
-      res.status(200).json(
-        _.reduce(
-          categories,
-          (result, cat) => {
-            return _.concat(
-              result,
-              _.map(cat.day.buckets, (day) => {
-                return {
-                  timestamp: new Date(day.key_as_string),
-                  category: cat.key,
-                };
-              })
-            );
-          },
-          []
-        )
-      );
-    });
-  });
-  router
-    .route(`${baseRoute}/last`)
-    .get(isUserAuthenticated, (req, res, next) => {
-      elastic("lastaudits", { id: req.params.id }, (err, results) => {
-        if (err) return next(err);
-        const categories = results.aggregations.categories.buckets;
-        res.status(200).json(
-          _.reduce(
-            categories,
-            (result, cat) => {
-              return _.chain(result)
-                .concat(
-                  result,
-                  _.map(cat.day.buckets, (day) => {
-                    return new Date(day.key_as_string);
-                  })
-                )
-                .uniqBy((date) => {
-                  return moment(date).format(DATE_FORMAT);
-                })
-                .take(5)
-                .value();
-            },
-            []
-          )
-        );
-      });
-    });
   router
     .route(`${baseRoute}/:date`)
     .get(isUserAuthenticated, (req, res, next) => {
@@ -65,14 +15,20 @@ const addRoutes = (router, baseRoute) => {
         {
           id: req.params.id,
           floor: req.params.date,
-          ceiling: moment(req.params.date, DATE_FORMAT)
-            .add(1, "days")
+          ceiling: dayjs(req.params.date, DATE_FORMAT)
+            .add(1, "day")
             .format(DATE_FORMAT),
-          format: "yyyyMMdd", // the ES format is different from the JS format
+          url: decodeURI(req.query.url),
         },
         (err, results) => {
           if (err) return next(err);
-          res.status(200).json(_.map(results.hits.hits, (hit) => hit._source));
+          res
+            .status(200)
+            .json(
+              results.aggregations.rules.buckets.map(
+                (bucket) => bucket.rules_hits.hits.hits[0]._source
+              )
+            );
         }
       );
     });
